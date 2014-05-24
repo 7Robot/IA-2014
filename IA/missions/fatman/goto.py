8eac48890e9bc4091a7810b1a1e9c96b94ac55c8
palkeo <contact@palkeo.com>
2014-05-24 17:49:12 +0200

    Mission goto pour fatman.

---
+++ IA/missions/fatman/goto.py
@@ -0,0 +1,32 @@
+from missions.mission import Mission
+
+class Goto(Mission):
+    def __init__(self, robot, boardth):
+        super().__init__(robot, boardth)
+        self.name = 'Goto mission'
+        self.position = None
+        self.angle = None
+        
+    def go(self, msg):
+        if (msg.board == "internal" and msg.name == 'goto'):
+            self.position = msg.position
+            self.angle = msg.angle
+            self.state = "going"            
+        
+        elif (self.state == "going"):
+            if (msg.board == "internal" and msg.name == 'alert'):
+                self.asserv.stop()
+                self.state = "waiting"
+            elif msg.board == "asserv" and msg.name == 'done':
+                self.state = 'turning'
+
+        elif self.state == 'turning':
+            if msg.board == 'asserv' and msg.name == 'done':
+                self.state = "off"
+                self.create_send_internal('done')
+                
+        elif (self.state == "waiting"):
+            if (msg.board == 'asserv' and msg.name == 'freepath'):
+                self.walk(self.axe, self.target)
+                self.state = "forward"
+                
