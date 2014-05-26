8e07f635affc490ea19c26eeba00cd3414a685fb
palkeo <contact@palkeo.com>
2014-05-26 18:48:48 +0200

    Revert "Essai de nouveau système pour les sick."
    This reverts commit 9183ef0011a86dcc029914644f56085182c42b3b.

--- IA/missions/fatman/goto.py
+++ IA/missions/fatman/goto.py
@@ -6,31 +6,29 @@ class Goto(Mission):
         self.name = 'Goto mission'
         self.position = None
         self.angle = None
-        self.sick = False
         
     def go(self, msg):
-        if msg.board == "internal" and msg.name == 'goto':
+        if (msg.board == "internal" and msg.name == 'goto'):
             self.position = msg.position
             self.angle = msg.angle
-            if not self.sick:
-                self.asserv.motion_pos(self.position[0], self.position[1])
+            self.asserv.motion_pos(self.position[0], self.position[1])
             self.state = "going"            
         
-        if msg.board == "asserv" and msg.name == 'sick':
-            self.asserv.stop()
-            self.sick = True
-
-        if self.state == 'going' and msg.board == "asserv" and msg.name == 'done' and not self.sick:
-            self.asserv.motion_angle(self.angle)
-            self.state = 'turning'
-
-        if self.state == 'turning' and msg.board == 'asserv' and msg.name == 'done' and not self.sick:
-            self.state = "off"
-            self.create_send_internal('goto done')
-                
-        if msg.board == 'asserv' and msg.name == 'freepath':
-            self.sick = False
-            if self.state == 'going':
-                self.asserv.motion_pos(self.position[0], self.position[1])
-            elif self.state == 'turning':
+        elif (self.state == "going"):
+            if (msg.board == "internal" and msg.name == 'alert'):
+                self.asserv.stop()
+                self.state = "waiting"
+            elif msg.board == "asserv" and msg.name == 'done':
                 self.asserv.motion_angle(self.angle)
+                self.state = 'turning'
+
+        elif self.state == 'turning':
+            if msg.board == 'asserv' and msg.name == 'done':
+                self.state = "off"
+                self.create_send_internal('goto done')
+                
+        elif (self.state == "waiting"):
+            if (msg.board == 'asserv' and msg.name == 'freepath'):
+                self.walk(self.axe, self.target)
+                self.state = "forward"
+                
