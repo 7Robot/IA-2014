b9365cac414bb90094dc94788916594ece35adc8
Ken <ken.hasselmann@etu.enseeiht.fr>
2014-05-27 09:59:01 +0200

    Update capteurs.py

--- IA/missions/robinch/capteurs.py
+++ IA/missions/robinch/capteurs.py
@@ -12,14 +12,14 @@ class Capteurs(Mission):
         logging.warn("Starting mission %s" % self.name)
         if self.state == 'on':
             if (msg.board == 'asserv' and msg.name == 'sick'):
-                if msg.id == 1:
+                if msg.id == 0:
                     self.create_send_internal('alert', id=0)
-                elif msg.id == 2:
+                elif msg.id == 1:
                     self.create_send_internal('alert', id=1)
             elif (msg.board == 'asserv' and msg.name == 'freepath'):
-                if msg.id == 1:
+                if msg.id == 0:
                     self.create_send_internal('freepath', id=0)
-                elif msg.id == 2:
+                elif msg.id == 1:
                     self.create_send_internal('freepath', id=1)
         elif self.state == 'off':
             self.state = 'on'
