34446e02be08c7eb6ccf607e1b5bb42c3aed621c
Ken <ken@kenh.fr>
2014-05-30 07:46:46 +0200

    fix typo

--- IA/missions/robinch/lances.py
+++ IA/missions/robinch/lances.py
@@ -35,7 +35,7 @@ class Lances(Mission):
             self.create_send_internal('turn', target=-11*pi/10)
             self.state = 'shoot'
 
-        elif (self.state == 'turning' and msg.name == 'doneLaunch'):
+        elif (self.state == 'turning2' and msg.name == 'doneLaunch'):
             self.create_send_internal('turn', target=11*pi/10)
             self.state = 'shoot'
             
